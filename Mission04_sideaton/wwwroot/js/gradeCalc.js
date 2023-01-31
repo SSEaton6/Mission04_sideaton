f44c02a79a98d613d89feaf3a1ec504e94de7b0e
Sidney Eaton <sideaton@byu.edu>
2023-01-30 23:18:02 -0700

    adding comments

--- Mission04_sideaton/wwwroot/js/gradeCalc.js
+++ Mission04_sideaton/wwwroot/js/gradeCalc.js
@@ -1,15 +1,16 @@
 ﻿$("#sendbtn").click( function () {
-   
+   // grabs the values input by the user and makes sure that they are numbers
     var assignments = Number($("#assignments").val());
     var quizzes = Number($("#quizzes").val());
     var gprojects = Number($("#groupProjects").val());
     var intex = Number($("#intex").val());
     var finalexam = Number($("#finalexam").val());
     var midexam = Number($("#midexam").val());
-   
+
+    // calculates final percent based on the weights provided in the syllabus
     var finalpercent = (assignments * .5) + (quizzes * .1) + (gprojects * .1) + (intex * .1) + (midexam * .1) + (finalexam * .1);
  
-    
+    // initializes the output string and assigns it a letter based on the grade. 
     outputstring = '';
     if (finalpercent >= 94) {
         outputstring = 'A'
@@ -44,5 +45,6 @@
     else {
         outputstring = 'E'
     }
+    // outputs the output string to the innerhtml of a p tag inside of the form.
     $("#grade").html("Grade: " + outputstring);
 })
