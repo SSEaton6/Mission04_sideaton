bb6787bf2193f285cd48e106a4a5b7fc818db8e2
Sidney Eaton <sideaton@byu.edu>
2023-01-30 23:07:07 -0700

    Add project files.

---
+++ Mission04_sideaton/wwwroot/js/gradeCalc.js
@@ -0,0 +1,48 @@
+﻿$("#sendbtn").click( function () {
+   
+    var assignments = Number($("#assignments").val());
+    var quizzes = Number($("#quizzes").val());
+    var gprojects = Number($("#groupProjects").val());
+    var intex = Number($("#intex").val());
+    var finalexam = Number($("#finalexam").val());
+    var midexam = Number($("#midexam").val());
+   
+    var finalpercent = (assignments * .5) + (quizzes * .1) + (gprojects * .1) + (intex * .1) + (midexam * .1) + (finalexam * .1);
+ 
+    
+    outputstring = '';
+    if (finalpercent >= 94) {
+        outputstring = 'A'
+    }
+    else if (finalpercent >= 90) {
+        outputstring = 'A-'
+    }
+    else if (finalpercent >= 87) {
+        outputstring = 'B+'
+    }
+    else if (finalpercent >= 84) {
+        outputstring = 'B'
+    }
+    else if (finalpercent >= 80) {
+        outputstring = 'B-'
+    }
+    else if (finalpercent >= 74) {
+        outputstring = 'C+'
+    }
+    else if (finalpercent >= 70) {
+        outputstring = 'C'
+    }
+    else if (finalpercent >= 67) {
+        outputstring = 'C-'
+    }
+    else if (finalpercent >= 64) {
+        outputstring = 'D+'
+    }
+    else if (finalpercent >= 60) {
+        outputstring = 'D-'
+    }
+    else {
+        outputstring = 'E'
+    }
+    $("#grade").html("Grade: " + outputstring);
+})
